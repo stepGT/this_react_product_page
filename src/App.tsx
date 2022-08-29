022441ac4660fb4bef94565289736b7b8a90aa8a
Andrey Zuev <stepgt@mail.ru>
2022-08-29 16:40:24 +1000

    Added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Product from './components/Product';
-import { products } from './components/data/products';
+import { IProduct } from './models';
+
+import './App.css';
 
 const App = () => {
+  const [data, setData] = useState<IProduct[]>([]);
+  async function fetchProduct() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+    setData(response.data);
+  }
+  useEffect(() => {
+    fetchProduct();
+  }, []);
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product item={products[0]} />
-      <Product item={products[1]} />
+      {data.map((el) => (
+        <Product key={el.id} item={el} />
+      ))}
     </div>
-  )
+  );
 };
 
 export default App;
