71b5d91d85e63655265c463d61c48b02280b7dc0
Andrey Zuev <stepgt@mail.ru>
2022-08-29 11:32:48 +1000

    Added tailwindcss && added component Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
 
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt iusto animi deserunt
+      quibusdam veritatis repellat, placeat vitae error officiis magni exercitationem eaque eligendi
+      quae maiores iure. Eum quaerat voluptate ipsa.
+    </div>
+  );
 };
 
 export default App;
