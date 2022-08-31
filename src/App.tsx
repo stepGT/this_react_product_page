7cc7843d10eb5d1a17db73992415f537c482ae25
Andrey Zuev <stepgt@mail.ru>
2022-08-31 14:37:40 +1000

    Loader centered

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
     close();
   };
   return (
-    <div className="container mx-auto max-w-2xl pt-5">
+    <div className="container h-screen mx-auto max-w-2xl pt-5">
       {loading && <Loader />}
       {error && <Error error={error} />}
       {!error && data.map((el) => <Product key={el.id} item={el} />)}
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import { Dna } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
-    <p className="flex justify-center">
+    <p className="flex justify-center items-center h-screen">
       <Dna
         visible={true}
         height="80"
